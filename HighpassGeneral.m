2069c516d438b5e4bcc4f843aaca9f5ac1b38769
Krista Perks <kperky@gmail.com>
2014-09-18 13:34:46 -0700

    update m files

--- HighpassGeneral.m
+++ HighpassGeneral.m
@@ -1,4 +1,4 @@
-function [data]=HighpassGeneral(data,basedata,fs)
+function [data]=HighpassGeneral(data,fs)
 %data must be in format rows=trials columns=data
 % data=BaselineGeneral(data,baselinewin,basedata);
 % shouldn't need to baseline because high pass removes DC offset
--- dnsample_data.m
+++ dnsample_data.m
@@ -1,14 +1,15 @@
 function data=dnsample_data(data,oldSampRate,newSampRate)
-didflip=0;
-if find(max(size(data)))==2
-    data=data';
-    didflip=1;
-end
+%for this to work, the data must be along one row, with columns as samples
+% didflip=0;
+% if find(max(size(data)))==1
+%     data=data';
+%     didflip=1;
+% end
 
 bin=round(oldSampRate/newSampRate);
 
-data=data(1:bin:end,1);
-if didflip==1
-    
-    data=data';
-end
+data=data(:,1:bin:end,1);
+% if didflip==1
+%     
+%     data=data';
+% end
